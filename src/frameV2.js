040dbd168e33cd738e7ec63c375b266cc8dadea0
Alastair Edmonds <alastair.edmonds@gmail.com>
2018-09-09 11:15:05 +0100

    create FrameV3, playing with dependency injection

--- src/frameV2.js
+++ src/frameV2.js
@@ -1,17 +1,18 @@
-function Grame() {
+function FrameV2() {
   this.inPlay = true 
   this.rollsArray = []
   this.totalPins = 0
 }
 
-Grame.prototype.inputRolls = function (roll1, roll2) {
+FrameV2.prototype.inputRolls = function (roll1, roll2) {
   if ((roll1.pins > 10) || (roll2.pins > 10)) {
     throw new Error('Error')
   } else if ((roll1.pins < 0) || (roll2.pins < 0)) {
     throw new Error('Error')
   } else {
     this.totalPins += roll1.pins
-    this.totalPins += roll2.pins 
+    this.totalPins += roll2.pins
+    this.rollsArray.push(roll1.pins, roll2.pins) 
   }
 }
 
