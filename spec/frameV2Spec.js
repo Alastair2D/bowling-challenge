ee1d6a5841a6d1c96d28b1442da17e9d4a2839c9
Alastair Edmonds <alastair.edmonds@gmail.com>
2018-09-09 12:41:19 +0100

    frameV2Spec - add gTests for #new

--- spec/frameV2Spec.js
+++ spec/frameV2Spec.js
@@ -3,7 +3,7 @@ describe("FrameV2", function() {
   beforeEach(function() {
     r1 = new Roll
     r2 = new Roll
-    r1.inputPinsHit(4)
+    r1.inputPinsHit(7)
     r2.inputPinsHit(2)
     frame = new FrameV2(r1, r2)
   })  
@@ -13,13 +13,15 @@ describe("FrameV2", function() {
     it('defaults as inPlay', function () {
       expect(frame.inPlay).toBe(true)
     })
+    it('takes roll1.pinsHit, roll2.pinsHit & sets them to ph1, ph2', function () {
+      expect(frame.ph1).toEqual(7)
+      expect(frame.ph2).toEqual(2)
+    })
     it('takes roll1.pinsHit and roll2.pinsHit & adds them to pinsHitArray', function () {
-      // r1 = new Roll 
-      // r2 = new Roll 
-      // r1.inputPinsHit(4)
-      // r2.inputPinsHit(2)
-      // frame = new FrameV2(r1, r2)
-      expect(frame.pinsHitArray).toEqual([4, 2]) 
+      expect(frame.pinsHitArray).toEqual([7, 2]) 
+    })
+    it('takes ph1 and ph2 and adds them to totalPinsHit', function () {
+      expect(frame.totalPinsHit).toEqual(9) 
     })
   })
 
