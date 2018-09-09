fa07007b19b3ffe12030932015539645efc0c446
Alastair Edmonds <alastair.edmonds@gmail.com>
2018-09-09 11:33:23 +0100

    injecting rolls

--- src/frameV3.js
+++ src/frameV3.js
@@ -1,16 +1,25 @@
 function FrameV3 (roll1, roll2) {
+  this.inPlay = true
   this.roll1 = roll1 
   this.roll2 = roll2
-  // this.rollsArray = []
-  // this.totalPins = 0
+  this.rollsArray = []
+  this.totalPins = 0
+}
+
+FrameV3.prototype.isComplete = function () {
+  if (this.rollsArray.length >= 2) {
+    this.inPlay = false
+    return true
+  } else {
+    return false
+  }
 }
 
 FrameV3.prototype.inputRolls = function (roll1, roll2) {
-  if ((roll1.pins > 10) || (roll2.pins > 10)) {
+  if (this.isComplete()) {
     throw new Error('Error')
-  } else if ((roll1.pins < 0) || (roll2.pins < 0)) {
-    throw new Error('Error')
-  } else {
+  }
+  else {
     this.totalPins += roll1.pins
     this.totalPins += roll2.pins
     this.rollsArray.push(roll1.pins, roll2.pins)
